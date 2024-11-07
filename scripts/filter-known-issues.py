871d7f7ef233ac575812eb8d979a594d5f944e3b
ZhangJianyu <zhang.jianyu@outlook.com>
2024-11-07 22:32:31 +0800

    debug

--- scripts/filter-known-issues.py
+++ scripts/filter-known-issues.py
@@ -144,7 +144,7 @@ arg_parser.add_argument("-e", "--errors", action="store", default=None,
 arg_parser.add_argument("-w", "--warnings", action="store", default=None,
                         help="file where to store warnings")
 arg_parser.add_argument("-c", "--config-dir", action="append", nargs="?",
-                        default=["known-issues/"],
+                        default=[".known-issues/"],
                         help="configuration directory (multiple can be "
                         "given; if none given, clears the current list) "
                         "%(default)s")
@@ -155,7 +155,7 @@ args = arg_parser.parse_args()
 logging.basicConfig(level=40 - 10 * (args.verbosity - args.quiet),
                     format="%(levelname)s: %(message)s")
 
-path = "known-issues/"
+path = ".known-issues/"
 logging.debug("Reading configuration from directory `%s`", path)
 print('args.config_dir', args.config_dir)
 config_import(args.config_dir)
