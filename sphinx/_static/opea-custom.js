e579124ef9f16a74551d455dac3ad8151fd62dcb
Ying Hu <ying.hu@intel.com>
2024-11-13 10:07:04 +0800

    Update opea-custom.js to fix235
    fix #235  the link to main page

--- sphinx/_static/opea-custom.js
+++ sphinx/_static/opea-custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
    /* tweak logo link to the marketing site instead of doc site */
-   $( ".icon-home" ).attr({href: "https://opea.dev/", target: "_blank"});
+   $( ".icon-home" ).attr({href: "https://opea-project.github.io/latest/index.html", target: "_blank"});
 
    /* open external links in a new tab */
    $('a[class*=external]').attr({target: '_blank', rel: 'noopener'});
